ad9392b1dec683f5e136e1aea28c489650e31222
anoop <anoopk.it@gmail.com>
2022-11-27 00:16:58 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,110 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Queues;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace DurableFunctionStorageCleaner;
+
+public class Program
+{
+
+    static void Main(string[] args)
+    {
+
+        string? StorageConnectionString = null;
+        string? StorageEntityPrefix = null;
+
+        try
+        {
+            Console.WriteLine("*** Start cleaning durable function storage Containers, queues and tables");
+            if (args != null && args.Length > 0)
+            {
+                StorageConnectionString = args[0];
+                StorageEntityPrefix = args.Length > 1 ? args[1] : null;
+            }
+            if (string.IsNullOrEmpty(StorageConnectionString))
+            {
+                Console.WriteLine(" *** Enter  StorageConnectionString: ");
+                StorageConnectionString = Console.ReadLine();
+            }
+
+            if (string.IsNullOrEmpty(StorageEntityPrefix))
+            {
+                Console.WriteLine(" *** Enter  StorageEntityPrefix: <Leave blank for default value: testhubname> ");
+                StorageEntityPrefix = Console.ReadLine();
+                StorageEntityPrefix = string.IsNullOrEmpty(StorageEntityPrefix) ? "testhubname" : StorageEntityPrefix;
+            }
+
+            try
+            {
+                Console.WriteLine("Deleting Durable function queues");
+                var queueServiceClient = new QueueServiceClient(StorageConnectionString);
+                var queues = queueServiceClient.GetQueues();
+                queues.ToList().ForEach(x =>
+                {
+                    if (x.Name.Contains(StorageEntityPrefix, StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        queueServiceClient.DeleteQueue(x.Name);
+                        Console.WriteLine($"** Queue deteted:  {x.Name}");
+                    }
+
+                });
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            try
+            {
+                Console.WriteLine("Deleting Durable function Blob containers");
+                var blobServiceClient = new BlobServiceClient(StorageConnectionString);
+                var containerList = blobServiceClient.GetBlobContainers();
+                containerList.ToList().ForEach(x =>
+                {
+                    if (x.Name.Contains(StorageEntityPrefix, StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        blobServiceClient.DeleteBlobContainer(x.Name);
+                        Console.WriteLine($"** Blob container deteted:  {x.Name}");
+                    }
+
+                });
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            try
+            {
+                Console.WriteLine("Deleting Durable function Tables");
+                CloudStorageAccount storageAccount = CloudStorageAccount.Parse(StorageConnectionString);
+                TableContinuationToken continuationToken = null;
+                var tableClient = storageAccount.CreateCloudTableClient();
+                var allTables = tableClient.ListTablesSegmentedAsync(continuationToken).Result;
+                allTables.ToList().ForEach(async x =>
+                {
+                    if (x.Name.Contains(StorageEntityPrefix, StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        await x.DeleteAsync();
+                        Console.WriteLine($"** Tables deteted:  {x.Name}");
+                    }
+
+
+                });
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+
+        Console.WriteLine("Completed cleaning up  Durable function storage");
+        Console.ReadLine();
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,54 @@
-# DurableFunctionStorageCleaner
-Clean blob, queue and table used by durable funtion
+## DurableFunctionStorageCleaner
+Cleans blob, queue and table used by durable funtion in one click
+
+### Use Case
+Durable Function restarts/resume on server restart. It manages cycle by own.
+While development we had to stop server in middle and run again. This triggers incomplete/not failed functions reasuming again. To avoid it only option is to 
+manually delete function storage two containers, five Queues and two tables from
+storage emulator.
+
+### Solution
+#### DurableFunctionStorageCleaner deletes the containers, queues and tables in one click.
+This is also helpful when we need to reset function app on server. The purge option of 
+Durable Functioin can't reset the hung operations or running functions.
+
+### Executing program 
+#### Download executable program to run
+> [Downlaod DurableFunctionStorageCleaner.exe](https://github.com/anoop0/DurableFunctionStorageCleaner/releases/download/1.0/DurableFunctionStorageCleaner.exe)
+
+### Option 1. 
+```
+> DurableFunctionStorageCleaner <Storage-Account-connection-string> <storage-pattern-prefix>
+```
+In local development <storage-pattern-prefix>  is usually testhubname. If your storage prefix is testhubname. You don't need to provide argument for <storage-pattern-prefix>
+
+### Option 2. 
+Just run and run window will give you options to provide value for parameter
+```
+> DurableFunctionStorageCleaner
+```
+You can also create batch files to feed input parameters
+```
+@echo off
+
+set /p cont=Enter Y to continue:
+
+if %cont% equ y (
+	ECHO Continue cleaning up storage: Blob Container, Queues and Tables
+
+	DurableFunctionStorageCleaner <Storage-Account-connection-string> <storage-pattern-prefix>
+
+)
+
+PAUSE
+```
+
+<span style="color:red">Be aware while providing connection string and storage names. It will delete them in one go </span>.
+
+## Example Run
+
+When running executable directly
+![Running executable](https://github.com/anoop0/DurableFunctionStorageCleaner/releases/download/1.0/Running.executable.png "Running executable")
+
+When running via batch file
+![Running executable](https://github.com/anoop0/DurableFunctionStorageCleaner/releases/download/1.0/Running.From.Batch.file.png "Running from batch file")
